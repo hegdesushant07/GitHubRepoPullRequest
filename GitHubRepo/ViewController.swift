4f1ebce1ed88c345b45628046e70806d904af3aa
Sushant Hegde <sushanthegde@Sushants-MacBook-Pro.local>
2023-02-08 13:46:16 +0530

    initial commit

---
+++ GitHubRepo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubRepo
+//
+//  Created by Sushant Hegde on 08/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
+    }
+
+
+}
+
