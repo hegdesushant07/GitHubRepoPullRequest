ac7a1cff44f854fbe8dcb1ccf12f1ee23378ec25
Sushant Hegde <sushanthegde@Sushants-MacBook-Pro.local>
2023-02-09 16:29:55 +0530

    Create NavigationController

---
+++ GitHubRepo/Features/PullRequestList/GitHubPullRequestsVC.swift
@@ -0,0 +1,20 @@
+//
+//  GitHubPullRequestsVC.swift
+//  GitHubRepo
+//
+//  Created by Sushant Hegde on 09/02/23.
+//
+
+import UIKit
+
+final class GitHubPullRequestsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
